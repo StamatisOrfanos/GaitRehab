87f249749c8970f2c142884cfc1ca18ffc2d634a
Stamatis Orfanos <stamatisorfanos99@gmail.com>
2025-04-22 23:48:26 +0700

    Initialise gait features

--- data_transform.py
+++ data_transform.py
@@ -3,6 +3,7 @@ import os
 import numpy as np
 import pandas as pd
 import scipy.signal as signal
+from scipy.signal import butter, filtfilt, find_peaks
 from scipy.fft import fft, fftfreq
 
 
@@ -24,7 +25,7 @@ def merge_data(data_dir, left_shank_path, right_shank_path, merge_type):
     # Read the CSV files
     left_shank_data  = pd.read_csv(left_shank_path)
     right_shank_data = pd.read_csv(right_shank_path)
-    
+        
     if merge_type == 'accelerometer':
         left_columns_names = {'elapsed (s)': 'left-elapsed (s)', 'x-axis (s)': 'left-x-axis (s)', 'y-axis (s)': 'left-y-axis (s)', 'z-axis (s)': 'left-z-axis (s)' }
         right_columns_names = {'elapsed (s)': 'right-elapsed (s)', 'x-axis (s)': 'right-x-axis (s)', 'y-axis (s)': 'right-y-axis (s)', 'z-axis (s)': 'right-z-axis (s)' }
@@ -116,7 +117,8 @@ def frequency_domain_features(data_dir, merge_type, output_path, fs=100, window_
     delta = pd.Timedelta(seconds=window_duration_sec)
     current_start = start_time
 
-    # Loop through the data in windows of 2 minutes / 120 seconds
+    # Loop through the data in windows of 2 minutes / 120 seconds and get the important frequency domain features
+    # Dominant frequency, Spectral entropy, Gait band energy
     while current_start + delta <= end_time:
         current_end = current_start + delta
         window = data[(data['timestamp (+0700)'] >= current_start) & (data['timestamp (+0700)'] < current_end)]
@@ -174,20 +176,95 @@ def gait_features(data_dir, merge_type):
     data.dropna(inplace=True)
     
     
-    # Calculate the gait metrics
+    # Calculate the gait metrics including stride times, stance/swing times, asymmetry index, and symmetry ratio
     left_peaks = signal.find_peaks(data['left-z-axis (deg/s)'], height=0.5, distance=100)
     right_peaks = signal.find_peaks(data['right-z-axis (deg/s)'], height=0.5, distance=100)
+    left_stride_times = np.diff(left_peaks[0])
+    right_stride_times = np.diff(right_peaks[0])
+    
+    left_stance_swing = detect_stance_swing(data['left-z-axis (deg/s)'], data['timestamp (+0700)'])
+    right_stance_swing = detect_stance_swing(data['right-z-axis (deg/s)'], data['timestamp (+0700)'])
+    
+    asymmetry = asymmetry_index(left_stride_times, right_stride_times)
+    symmetry = symmetry_ratio(left_stride_times, right_stride_times)
+    gait_metrics = {
+        'left_stride_times': left_stride_times,
+        'right_stride_times': right_stride_times,
+        'left_stance_swing': left_stance_swing,
+        'right_stance_swing': right_stance_swing,
+        'asymmetry_index': asymmetry,
+        'symmetry_ratio': symmetry
+    }
+    
+    # Save the gait metrics to a CSV file
+    gait_metrics_df = pd.DataFrame(gait_metrics)
+    gait_metrics_df.to_csv(os.path.join(data_dir, 'gait_metrics.csv'), index=False)
     
 
-from scipy.signal import find_peaks
 
-def extract_stride_times(z_filtered, time):
-    peaks, _ = find_peaks(z_filtered, distance=20)  # distance depends on sampling rate
-    stride_times = np.diff(time[peaks])
-    return stride_times, peaks
+def extract_cross_limb_features_safe(data, fs=100):
+    data['left_z_filtered'] = butter_low_pass(data['left-z-axis (deg/s)'], fs=fs)
+    data['right_z_filtered'] = butter_low_pass(data['right-z-axis (deg/s)'], fs=fs)
+    
+    left_peaks, _ = find_peaks(data['left_z_filtered'], distance=fs*0.5)
+    right_peaks, _ = find_peaks(data['right_z_filtered'], distance=fs*0.5)
+    
+    features = []
+    for i in range(min(len(left_peaks), len(right_peaks)) - 1):
+        l_start, l_end = left_peaks[i], left_peaks[i+1]
+        r_start, r_end = right_peaks[i], right_peaks[i+1]
+
+        l_cycle = data.iloc[l_start:l_end]
+        r_cycle = data.iloc[r_start:r_end]
+
+        # Truncate to shortest cycle
+        min_len = min(len(l_cycle), len(r_cycle))
+        if min_len < 5:
+            continue  # skip very short cycles to avoid noise
+
+        left_stride_duration  = data['left-elapsed (s)'].iloc[l_end]  - data['left-elapsed (s)'].iloc[l_start]
+        right_stride_duration = data['right-elapsed (s)'].iloc[r_end] - data['right-elapsed (s)'].iloc[r_start]
+
+        f = {
+            "left_stride_duration": left_stride_duration,
+            "right_stride_duration": right_stride_duration,
+            "stride_duration_diff": abs(left_stride_duration - right_stride_duration),
+            "stride_duration_symmetry_ratio": min(left_stride_duration, right_stride_duration) / max(left_stride_duration, right_stride_duration),
+            "left_peak": data['left_z_filtered'].iloc[l_start:l_end].max(),
+            "right_peak": data['right_z_filtered'].iloc[r_start:r_end].max(),
+            "peak_diff": abs(data['left_z_filtered'].iloc[l_start:l_end].max() - df['right_z_filtered'].iloc[r_start:r_end].max()),
+            "z_corr": np.corrcoef(
+                l_cycle['left_z_filtered'].values[:min_len],
+                r_cycle['right_z_filtered'].values[:min_len]
+            )[0,1]
+        }
+        features.append(f)
+
+    return pd.DataFrame(features)  
+
+
+def butter_low_pass(data, cutoff=6, fs=100, order=2):
+    '''
+    Apply a low-pass Butterworth filter to the data.
+    Args:
+        data (np.array): Input data to be filtered.
+        cutoff (float): Cutoff frequency in Hz.
+        fs (int): Sampling frequency in Hz.
+        order (int): Order of the filter.
+    '''
+    nyq = 0.5 * fs
+    normal_cutoff = cutoff / nyq
+    b, a = butter(order, normal_cutoff, btype='low', analog=False)
+    return filtfilt(b, a, data)
 
 
 def detect_stance_swing(z_filtered, time):
+    '''
+    Detect stance and swing phases from the gyroscope data.
+    Args:
+        z_filtered (np.array): Filtered z-axis gyroscope data.
+        time (np.array): Corresponding time values.
+    '''
     zero_crossings = np.where(np.diff(np.sign(z_filtered)))[0]
     phases = []
     for i in range(len(zero_crossings) - 1):
@@ -201,7 +278,19 @@ def detect_stance_swing(z_filtered, time):
     return phases
 
 def asymmetry_index(left, right):
+    '''
+    Calculate the asymmetry index between left and right stride times.
+    Args:
+        left (list): Left stride times.
+        right (list): Right stride times.
+    '''
     return [(l - r) / (l + r) if (l + r) != 0 else 0 for l, r in zip(left, right)]
 
 def symmetry_ratio(left, right):
+    '''
+    Calculate the symmetry ratio between left and right stride times.
+    Args:
+        left (list): Left stride times.
+        right (list): Right stride times.
+    '''
     return [min(l, r) / max(l, r) if max(l, r) != 0 else 0 for l, r in zip(left, right)]
