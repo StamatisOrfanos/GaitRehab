423c41b131793fc1f1e709cf817b7225dfa8dba6
Stamatis Orfanos <stamatisorfanos99@gmail.com>
2025-05-16 15:44:46 +0700

    Fix new data gyroscope format

--- test.py
+++ test.py
@@ -1,8 +1,26 @@
-patient_path = 'Data/Healthy/Patient_1'
+import pandas as pd
+import os
+from datetime import datetime, timedelta
+
+def fix_format(path):
+    df = pd.read_csv(path, index_col=0)
+
+    # Rename columns
+    df = df.rename(columns={'Gyro1x': 'x-axis (deg/s)', 'Gyro1y': 'y-axis (deg/s)', 'Gyro1z': 'z-axis (deg/s)', 
+                            'Time': 'timestamp (+0700)', 'microsecond': 'micro', 'elasped_time': 'elapsed (s)'})
+
+    df["elapsed (s)"]       = (df["micro"] - df["micro"].iloc[0]) / 1000
+    base_datetime           = datetime.strptime("2024-08-21 09:35:00.000", "%Y-%m-%d %H:%M:%S.%f")
+    df["timestamp (+0700)"] = df["elapsed (s)"].apply(lambda s: base_datetime + timedelta(seconds=s))
+    df["epoc (ms)"]         = df["timestamp (+0700)"].apply(lambda x: x.timestamp() * 1000)
+    df['x-axis (deg/s)']    = df['x-axis (deg/s)'].round(3)
+    df['y-axis (deg/s)']    = df['y-axis (deg/s)'].round(3)
+    df['z-axis (deg/s)']    = df['z-axis (deg/s)'].round(3)
 
 
-
-patiend_id = patient_path.split('/')[-1].lower()
-status = 0 if patient_path.__contains__('Healthy') else 1
-patient_id = patiend_id + '_' + str(status)
-print(patient_id)
+    df_order = df[['epoc (ms)', 'timestamp (+0700)', 'elapsed (s)', 'x-axis (deg/s)', 'y-axis (deg/s)', 'z-axis (deg/s)']]
+    df_order.to_csv('LeftShank-Gyroscope.csv', index=False)
+    
+    
+path = 'Data/Stroke/Patient_8/RightShank-Gyroscope.csv'
+fix_format(path)
