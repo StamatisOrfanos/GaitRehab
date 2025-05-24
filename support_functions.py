dffaf95807ad9ab5e3ca62694789ef24c7d5976e
Stamatis Orfanos <stamatisorfanos99@gmail.com>
2025-05-24 18:51:45 +0800

    Try to visualise the detection model results

--- support_functions.py
+++ support_functions.py
@@ -20,10 +20,25 @@ def time_domain_features(data: pd.DataFrame):
 
     return model_input
 
-def summarize(lst):
+def summarize(lst: list):
+    '''
+    Calculate the mean of a list.
+    Args:
+        lst (list): List of values.
+    '''
     return float(np.mean(lst)) if len(lst) > 0 else 0
 
-def simulate_gait_signal(amplitude, freq, phase_shift, noise, duration_sec=2.0, fs=100):
+def simulate_gait_signal(amplitude: float, freq: float, phase_shift: float, noise: float, duration_sec=2.0, fs=100):
+    '''
+    Simulate a gait signal with noise.
+    Args:
+        amplitude (float): Amplitude of the sine wave.
+        freq (float): Frequency of the sine wave.
+        phase_shift (float): Phase shift of the sine wave.
+        noise (float): Standard deviation of the Gaussian noise.
+        duration_sec (float): Duration of the signal in seconds.
+        fs (int): Sampling frequency in Hz.
+    '''
     t = np.linspace(0, duration_sec, int(fs * duration_sec))
     signal = amplitude * np.sin(2 * np.pi * freq * t + phase_shift)
     return signal + np.random.normal(0, noise, size=t.shape)
